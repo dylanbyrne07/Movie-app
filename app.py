3bf76ac8895341b5f140ae5c4364faa77086adb3
dlbcreations85 <dlbcreations85@gmail.com>
2022-09-14 12:02:23 +0100

    have titles and images for each of the movies loaded

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request
-import random
+from flask import Flask, render_template, request, jsonify
+import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
-
-def rand_int():
-    rand_int = random.randrange(0, 100)
-    return "<p>hello</p>"
-    
+    popular_movies_data = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=ca0668e9a773ee0bddc2b9e3a7fdacc7&language=en-US&page=1")
+    popular_movies = json.loads(popular_movies_data.content)
+    print(popular_movies)
+    for movie in popular_movies['results']:
+        print(movie['original_title'])
+    return render_template('index.html', popular_movies=popular_movies)
