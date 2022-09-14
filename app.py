84e2f7b14dd2bbeb3133640d94d945128a818103
dlbcreations85 <dlbcreations85@gmail.com>
2022-09-14 21:48:04 +0100

    have it now that when film card is clicked it opens the route for the individual film and makes and api call to get all the data about that film

--- app.py
+++ app.py
@@ -15,4 +15,14 @@ def home():
 
 @app.route('/movie/<int:Movie_id>')
 def film_page(Movie_id):
-    return f"your movie id is {str(Movie_id)}"
+    movie_id_str = str(Movie_id)
+    api = f"https://api.themoviedb.org/3/movie/{Movie_id}?api_key=ca0668e9a773ee0bddc2b9e3a7fdacc7&language=en-US"
+    #get json data from the api and pass it into the html page so that it can be accesed and printed to screen
+    print(type(api))
+    film_data_raw = requests.get(api)
+    print(film_data_raw)
+    film_data = "hello"
+    film_data = json.loads(film_data_raw.content)
+    
+    return render_template('movie_page.html', film_data=film_data)
+
