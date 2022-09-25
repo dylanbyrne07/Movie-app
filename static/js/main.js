d36e381cf2c0ae991eae61e8fe2d97f990f4b3ab
dlbcreations85 <dlbcreations85@gmail.com>
2022-09-25 17:27:34 +0100

    have everyting working, and hamburger animation inplace just need to make the menu appear now

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    const menu_btn = document.querySelector('.hamburger');
+
+    menu_btn.addEventListener('click', function() {
+        menu_btn.classList.toggle("is-active");
+    })
+}
